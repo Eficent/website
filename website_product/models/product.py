dad3d01e1ba7a2444869a283758099fc1ebe6da3
hveficent <hector.villarreal@eficent.com>
2018-08-08 10:29:53 +0200

    [WIP] [11.0] website_product no website_sale dependency

--- website_product/models/product.py
+++ website_product/models/product.py
@@ -1,18 +1,26 @@
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-from odoo import api, models
+from odoo import api, fields, models
 from odoo.addons.http_routing.models.ir_http import slug
+from odoo.tools.translate import html_translate
 
 
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+class WebsiteProductTemplate(models.Model):
+    _inherit = ['product.template',
+                'website.seo.metadata',
+                'website.published.mixin']
+
+    website_description = fields.Html('Description for the website',
+                                      sanitize_attributes=False,
+                                      translate=html_translate)
 
     @api.multi
     def _compute_website_url(self):
-        super(ProductTemplate, self)._compute_website_url()
+        super(WebsiteProductTemplate, self)._compute_website_url()
         # If the product cannot be sold, then use the new form view.
         # Otherwise, use the defult URL from website_sale, which will
         # redirect the user to the shop.
-        for product in self.filtered(lambda p: not p.sale_ok):
+        for product in self.filtered(
+                lambda p: not p.sale_ok and p.website_published):
             product.website_url = "/product/%s" % slug(product)
