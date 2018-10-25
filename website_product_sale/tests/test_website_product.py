f870423f18205769d13833c5700c9587204492aa
hveficent <hector.villarreal@eficent.com>
2018-10-25 12:45:04 +0200

    [ADD] 11.0 website_product_sale

---
+++ website_product_sale/tests/test_website_product.py
@@ -0,0 +1,36 @@
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from mock import patch
+from odoo.tests.common import SavepointCase
+from odoo.addons.website_product.controllers.main import WebsiteProductPage
+
+
+class TestController(SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestController, cls).setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls.uom_unit = cls.env.ref('product.product_uom_unit')
+
+        cls.product_sale_ok = cls.env['product.template'].create({
+            'name': 'Product sale ok',
+            'uom_id': cls.uom_unit.id,
+            'uom_po_id': cls.uom_unit.id,
+            'sale_ok': True,
+        })
+
+        cls.product_no_sale = cls.env['product.template'].create({
+            'name': 'Product no sale',
+            'uom_id': cls.uom_unit.id,
+            'uom_po_id': cls.uom_unit.id,
+            'sale_ok': False,
+        })
+
+    def test_compute_website_url(self):
+        """Test website_url from product.templates if sellable or not."""
+        self.assertFalse('/shop/' in self.product_no_sale.website_url)
+        self.assertTrue('/shop/' in self.product_sale_ok.website_url)
+
